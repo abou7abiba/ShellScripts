1b6e06c9c22ed12f5b11f283fd2c7e6af9f7e135
Ahmed Ibrahim <abou7abiba@gmail.com>
2020-08-12 18:17:01 +0200

    Adding Nvidia-CUDA support scripts initial script.

---
+++ nvidia/nvidia-gpu/cuda-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is to install Nvidia CUDA 10.1 on Ubuntu 20.04
+#
+# References:
+# Ref1: Installing CUDA 10.1 on Ubuntu 20.04 
+#       https://medium.com/@stephengregory_69986/installing-cuda-10-1-on-ubuntu-20-04-e562a5e724a0
